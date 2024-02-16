3c27e9b0388b5f33d68d0c8221a264cb3a0c03f6
kmaitreys <peakcipher1729@gmail.com>
2024-02-16 23:02:44 +0530

    juliaup

--- project/src/solver.jl
+++ project/src/solver.jl
@@ -22,6 +22,37 @@ function solve_diffusion_equation(n, m, Δt, Δx, D, u0, T)
     return u
 end
 
+"""
+    solve_diffusion_equation!(u, Δt, Δx, D, u0, T)
+
+Solve the diffusion equation u_t = D u_xx on the interval [0, T] with Dirichlet boundary conditions
+u(0, t) = u0(0), u(T, t) = u0(T) and initial condition u(x, 0) = u0(x).
+
+# Arguments
+- `u::Array{Float64,2}`: The solution array
+- `Δt::Float64`: The time step
+- `Δx::Float64`: The spatial step
+- `D::Float64`: The diffusion coefficient
+- `u0::Function`: The initial condition
+- `T::Float64`: The final time
+
+# Example
+```julia
+n, m = 100, 100
+Δt, Δx = 0.01, 0.01
+D = 0.1
+u0(x) = exp(-x^2)
+T = 1.0
+u = zeros(n, m)
+solve_diffusion_equation!(u, Δt, Δx, D, u0, T)
+```
+
+# References
+- https://en.wikipedia.org/wiki/Finite_difference_method
+
+# See also
+- `solve_diffusion_equation`
+"""
 function solve_diffusion_equation!(u, Δt, Δx, D, u0, T)
     # Create the grid
     n, m = size(u)
