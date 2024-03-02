3d619b96aeb8921e8e5ebc5a7efab576e01f5919
kmaitreys <peakcipher1729@gmail.com>
2024-03-02 17:16:37 +0530

    plots working now

--- project/src/solver.jl
+++ project/src/solver.jl
@@ -4,8 +4,15 @@ using Plots
 ## Task 1
 ## 1.1
 # Solving the diffusion equation in `r` (under the no-Z) approximation
-function gradient(arr::Vector{Float64}, dr)
-    return (arr[2:end] .- arr[1:end-1]) / dr
+function gradient(arr, dr)
+    n = length(arr)
+    grad = similar(arr)
+    grad[1] = (arr[2] - arr[1]) / dr
+    grad[end] = (arr[end] - arr[end-1]) / dr
+    for i in 2:length(arr)-1
+        grad[i] = (arr[i+1] - arr[i-1]) / (2 * dr)
+    end
+    return grad
 end
 
 
@@ -13,24 +20,22 @@ function get_laplacian(y::Vector{Float64}, dr)
     return gradient(gradient(y, dr), dr)
 end
 
-function runge_kutta_fourth_order!(y::Vector{Float64}, dr, η_t)
-    # println("y: ", y)
+function runge_kutta_fourth_order!(y::Vector{Float64}, dr, dt, η_t)
     k1 = dt * η_t * get_laplacian(y, dr)
-    println(size(k1))
     k2 = dt * η_t * get_laplacian(y + 0.5 * k1, dr)
     k3 = dt * η_t * get_laplacian(y + 0.5 * k2, dr)
     k4 = dt * η_t * get_laplacian(y + k3, dr)
-    y += (k1 + 2 * k2 + 2 * k3 + k4) / 6
-    return y
+    y .+= (k1 + 2 * k2 + 2 * k3 + k4) / 6
 end
 
 function evolve_magnetic_field(R, T, Nr, Nt, η_t)
     dr = R / (Nr - 1)
+    dt = T / Nt
     R_vals = LinRange(0, R, Nr)
     B_r = @. sin(4 * π * R_vals / R)
     evolution_record = zeros(Nr, Nt)
     for i in 1:Nt
-        runge_kutta_fourth_order!(B_r, dr, η_t)
+        runge_kutta_fourth_order!(B_r, dr, dt, η_t)
         evolution_record[:, i] .= B_r
     end
     return evolution_record
@@ -47,14 +52,25 @@ T = 5.0
 Nt = 500
 # Run simulation
 evolution_record = evolve_magnetic_field(R, T, Nr, Nt, η_t)
-
 # Plots 
 
 # Time evolution of the magnetic field
 
 T = LinRange(0, T, Nt)
-plot(T, evolution_record, label="Magnetic field evolution", xlabel="Time", ylabel="Magnetic field")
 
+function plot_every_10th_row(data::Matrix)
+    num_rows = size(data, 1)
+    p = plot(legend=:topleft)  # Initialize plot object
+    for i in 1:20:num_rows
+        plot!(p, abs.(data[i, :]), label="Row $i")
+    end
+    xlabel!("Index")
+    ylabel!("Value")
+    title!("Plot of Every 10th Row")
+    return p
+end
+
+plot_every_10th_row(evolution_record)
 
 
 
