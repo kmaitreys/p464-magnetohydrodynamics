31e39011925a8b35342151a4913b2aaef7cda4ac
kmaitreys <peakcipher1729@gmail.com>
2024-03-31 19:26:11 +0530

    update project

--- project/src/GalacticDynamo.jl
+++ project/src/GalacticDynamo.jl
@@ -1,11 +1,9 @@
 # Solving the galactic dynamo using finite difference Runge-Kutta methods
-# module GalacticDynamo
+module GalacticDynamo
 
 using Random
 using Plots
-## Task 1
-## 1.1
-# Solving the diffusion equation in `r` (under the no-Z) approximation
+
 function gradient(arr, dr)
     n = length(arr)
     grad = similar(arr)
@@ -43,72 +41,33 @@ function evolve_magnetic_field(R, T, Nr, Nt, η_t)
     return evolution_record
 end
 
-# Parameters
-η_t = 0.5 # Scaled turbulent magnetic diffusivity
 
-# Radial grid
-R = 10.0
-Nr = 200
-# Time
-T = 5.0
-Nt = 500
-# Run simulation
-evolution_record = evolve_magnetic_field(R, T, Nr, Nt, η_t)
-# Plots 
 
-# Time evolution of the magnetic field
-T = LinRange(0, T, Nt)
-function plot_every_10th_row(data::Matrix)
-    num_rows = size(data, 1)
-    p = plot(legend=:topleft)  # Initialize plot object
-    for i in 1:20:num_rows
-        plot!(p, abs.(data[i, :]), label="Row $i")
-    end
-    xlabel!("Index")
-    ylabel!("Value")
-    title!("Plot of Every 10th Row")
-    return p
+"""
+    magnetic_field_radial(B_r, B_ϕ, V_r, V_z, scale_height, α, dr, dt, η)
+
+Solve the radial component of the magnetic field using the following equation:
+
+# Arguments
+- `B_r::Vector{Float64}`: The radial component of the magnetic field
+- `B_ϕ::Vector{Float64}`: The azimuthal component of the magnetic field
+- `V_r::Vector{Float64}`: The radial velocity of the gas
+- `V_z::Vector{Float64}`: The vertical velocity of the gas
+- `scale_height::Float64`: The scale height of the gas
+- `α::Float64`: The alpha parameter
+- `dr::Float64`: The radial step size
+- `dt::Float64`: The time step size
+- `η::Float64`: The magnetic diffusivity
+
+"""
+function magnetic_field_radial(B_r, B_ϕ, V_r, V_z, scale_height, α, dr, dt, η)
+    evolution_record = zeros(length(B_r) + 2)
+
+
+
+    evolution_record[2:end-1] = dt
 end
 
-plot_every_10th_row(evolution_record)
 
 
-# function decay(t, A, τ)
-#     return A * exp(-t / τ)
-# end
-
-# function get_pitch_angle(B_r, B_φ)
-#     return atan(B_φ, B_r)
-# end
-
-# @. decay(t, A, τ) = A * exp(-t / τ)
-# time_points = LinRange(0, 5, 100)
-# magnetic_field_magnitude = decay
-
-
-
-
-
-
-# function init_magnetic_field(R)
-#     seed = rand
-# end
-
-
-# Importing the Random module
-# using Random
-
-# # Array of strings containing 'sin' and 'cos'
-# functions = ["sin", "cos"]
-
-# # Assuming R is an array or sequence
-# R = [1, 2, 3, 4, 5]  # Example array, replace this with your actual data
-
-# # Generating random choices
-# random_choices = rand(functions, length(R))
-
-# # Printing the result
-# println(random_choices)
-
-
-# end
+end
